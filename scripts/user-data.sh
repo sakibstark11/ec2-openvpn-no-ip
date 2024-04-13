2f086ed7ae283dc4a5f31a81ebe0085294825b28
sakibstark11 <16sakib@gmail.com>
2024-04-13 13:20:08 +0100

    fixing openvpn install command

--- scripts/user-data.sh
+++ scripts/user-data.sh
@@ -8,13 +8,25 @@ yum install -y git curl https://dl.fedoraproject.org/pub/epel/epel-release-lates
 yum install -y noip
 
 systemctl enable noip.service
-
 noip2 -C -u ${noip_username} -p ${noip_password} -U 1
-
 systemctl start noip.service
 
 su - ec2-user
+cd /home/ec2-user
+
 curl -O ${openvpn_script}
 chmod +x openvpn-install.sh
 
-APPROVE_INSTALL=y ENDPOINT=${noip_domain} CLIENT=portable-network PASS=1 ./openvpn-install.sh
+export APPROVE_INSTALL=y
+export ENDPOINT=${noip_domain}
+export CLIENT=portable-network
+export PASS=1
+export APPROVE_IP=n
+export IPV6_SUPPORT=n
+export PORT_CHOICE=1
+export PROTOCOL_CHOICE=1
+export DNS=1
+export COMPRESSION_ENABLED=n
+export CUSTOMIZE_ENC=n
+
+./openvpn-install.sh
