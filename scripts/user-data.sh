9ac2c8ed8c9af1c665513417eb68d1952481e419
sakibstark11 <16sakib@gmail.com>
2024-04-13 17:31:24 +0100

    use ami to create carbon copy images

--- scripts/user-data.sh
+++ scripts/user-data.sh
@@ -2,7 +2,11 @@
 
 set -e
 
-apt update
+wget https://s3.amazonaws.com/ec2-downloads-windows/SSMAgent/latest/debian_amd64/amazon-ssm-agent.deb
+dpkg -i amazon-ssm-agent.deb
+systemctl enable amazon-ssm-agent
+systemctl start amazon-ssm-agent
+
 apt install -y gcc wget curl tar
 
 wget https://dmej8g5cpdyqd.cloudfront.net/downloads/noip-duc_3.0.0.tar.gz
