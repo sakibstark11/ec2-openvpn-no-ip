9cb212abfec95816c9ac495a6b2251462adb79a7
sakibstark11 <16sakib@gmail.com>
2024-07-08 20:02:44 +0100

    change instance type

--- variables.tf
+++ variables.tf
@@ -35,5 +35,5 @@ variable "openvpn_script" {
 variable "instance_type" {
   type        = string
   description = "type of instance you want to run"
-  default     = "t2.nano"
+  default     = "t4g.nano"
 }
